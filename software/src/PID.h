48b0bc3b6203ae5d03cc4f47e0d3d86c0be8ae3b
SashaBrooke <123516043+SashaBrooke@users.noreply.github.com>
2024-12-03 23:24:56 +1100

    Implement basic PID controller with initial testing for simple position control

---
+++ software/src/PID.h
@@ -0,0 +1,34 @@
+#ifndef PID_H
+#define PID_H
+
+// TODO: Documentation
+
+typedef struct {
+    float Kp;
+    float Ki;
+    float Kd;
+
+    float tau;
+
+    float outLimMin;
+    float outLimMax;
+
+    float intLimMin;
+    float intLimMax;
+
+    float T;
+
+    float integrator;
+    float prevError;
+    float differentiator;
+    float prevMeasurement;
+
+    float output;
+} PID_t;
+
+void PID_Init(volatile PID_t *pid, float Kp, float Ki, float Kd, float tau, float outLimMin,
+                float outLimMax, float intLimMin, float intLimMax, float T);
+
+float PID_Update(volatile PID_t *pid, float setpoint, float measurement);
+
+#endif
--- software/src/encoder.c
+++ software/src/encoder.c
@@ -15,10 +15,12 @@
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
 
-#define I2C_PORT i2c0             
-#define DIR_PIN 3                
-#define I2C_SDA_PIN 4          
-#define I2C_SCL_PIN 5    
+#include "PID.h"
+
+#define I2C_PORT i2c0
+#define DIR_PIN 3
+#define I2C_SDA_PIN 4
+#define I2C_SCL_PIN 5
 
 #define TEST_PIN 0
 
@@ -29,6 +31,8 @@ static int addr = 0x36;         /**< I2C address of the AS5600 encoder (can only
 
 volatile uint16_t combined_raw_angle = 0;
 volatile bool print_flag = false;
+volatile PID_t positionController;
+volatile float pid_output = 0.0f;
 
 /**
  * @brief Initializes the encoder and checks the magnet's status.
@@ -78,15 +82,18 @@ void encoder_init() {
 }
 
 /**
- * @brief Reads the raw angle data from the AS5600 magnetic encoder and manages a periodic print flag.
+ * @brief Reads the raw angle data from the AS5600 magnetic encoder and calculates a PID output.
  *
  * This function is a callback for a repeating timer. It communicates with the AS5600 encoder over I2C
- * to read the raw angle value and updates a counter. Every 100 calls, it sets a flag (`print_flag`) to
- * signal the main loop to print the angle value. The function also toggles a GPIO pin (`TEST_PIN`)
- * for debugging purposes to measure timing.
+ * to read the raw angle value and calculates a PID response. Every 100 calls, it sets a flag 
+ * (`print_flag`) to  * signal the main loop to print the angle value. The function also toggles a GPIO 
+ * pin (`TEST_PIN`) for debugging purposes to measure timing.
  *
  * @param timer Pointer to the repeating timer structure. Unused in this function.
  * @return true Always returns true to keep the timer active.
+ * 
+ * @note Encoder raw angle read and PID calculation takes approximately 150us. Could probably only push
+ *       timer to minimum 200us between repetitions to be safe.
  */
 bool read_raw_angle(repeating_timer_t *timer) {
     gpio_put(TEST_PIN, 1); // Pin toggle for debug
@@ -99,6 +106,9 @@ bool read_raw_angle(repeating_timer_t *timer) {
 
     // Shift the lower 4 bits from the high byte and combine with the 8 bits from the low byte
     combined_raw_angle = ((raw_angle[0] & 0x0F) << 8) | raw_angle[1];
+
+    // Calculate PID response demanding a setpoint raw angle of 1000
+    pid_output = PID_Update(&positionController, 1000.0f, (float)combined_raw_angle);
 
     // Track the number of calls to this function and print at multiples of 100
     static int counter = 0; 
@@ -135,13 +145,15 @@ int main() {
 
     encoder_init();
 
+    PID_Init(&positionController, 1.0f, 0.0f, 0.0f, 0.0f, -1000.0f, 1000.0f, 0.0f, 0.0f, 0.0015f);
+
     struct repeating_timer timer; // Maybe use real time clock peripheral if for use longer than ~72 mins
     add_repeating_timer_us(-1500, read_raw_angle, NULL, &timer);
 
     while(true) {
         if (print_flag) {
+            printf("Raw angle: [%u]\tPID Output: [%f]\n", combined_raw_angle, pid_output);
             print_flag = false;  /**< Reset print flag */
-            printf("%u\n", combined_raw_angle);
         }
     }
 
